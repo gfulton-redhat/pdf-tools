0434741f1efa4137ff1c7728d43c1f41fa08d61d
gfulton-redhat <gfulton@redhat.com>
2024-01-12 17:34:59 -0800

    frame divisor and total number of frames

--- src/main/java/org/adoption/Loader.java
+++ src/main/java/org/adoption/Loader.java
@@ -3,6 +3,7 @@ package org.adoption;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 
 import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.deeplearning4j.nn.api.Model;
@@ -13,7 +14,7 @@ import java.net.URI;
 import static org.deeplearning4j.nn.modelimport.keras.KerasModelImport.*;
 
 public interface Loader {
-     Logger LOGGER = LoggerFactory.getLogger(Loader.class);
+    Logger LOGGER = LoggerFactory.getLogger(Loader.class);
 
     default Model getModel(URI uri) throws Exception {
         try {
@@ -35,4 +36,21 @@ public interface Loader {
         }
     }
 
+    /**
+     * Get the total number of frames and calculate the frame divisor, rounded to the closest int.
+     * @return totalNumberOfFrames
+     */
+    default int getTotalNumberOfFrames(VideoCapture videoCapture){
+        int totalNumberOfFrames = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_COUNT);
+        return totalNumberOfFrames;
+    }
+
+    /**
+     * Get calculate the frame divisor and rounded to the closest int.
+     * @return frameDivisor
+     */
+    default int getFrameDivisor(VideoCapture videoCapture, double period) {
+        int frameDivisor = (int) Math.round(period * videoCapture.get(Videoio.CAP_PROP_FPS));
+        return frameDivisor;
+    }
 }
